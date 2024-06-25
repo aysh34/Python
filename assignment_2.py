41d069c2f83bed1a87f5227e63ebdb591ad40b6f
Ayesha Saleem <ayeshasaleem853@gmail.com>
2024-06-25 12:10:05 +0500

    assignment_2

---
+++ assignment_2.py
@@ -0,0 +1,29 @@
+# AND Operation Problem:
+# Calculate the result of the bitwise AND operation on the numbers 25 and 30.
+and_op_result = 25 & 30
+print(and_op_result)  # 24
+
+# OR Operation Problem:
+# Calculate the result of the bitwise OR operation on the numbers 45 and 25.
+or_op_result = 45 | 25
+print(or_op_result)  # 61
+
+# XOR Operation Problem:
+# Calculate the result of the bitwise XOR operation on the numbers 29 and 15
+xor_op_result = 29 ^ 15
+print(xor_op_result)  # 18
+
+# NOT Operation Problem:
+# Calculate the result of the bitwise NOT operation on the number 42.
+not_op_result = ~42
+print(not_op_result)  # -43
+
+# Shift Operations Problem:
+# Calculate the result of left shifting the number 15 by 3 positions.
+left_shift = 15 << 3
+print(left_shift)  # 120
+
+# Shift Operations Problem:
+# Calculate the result of right shifting the number 200 by 4 positions.
+right_shift = 200 >> 4
+print(right_shift)  # 12
